076f20816091e914c8cc78caec28b824f86526ae
bryzZz <tebloev.nikita@mail.ru>
2022-02-19 18:37:58 +0800

    remove svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from '@honkhonk/vite-plugin-svgr';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react()],
     resolve: {
         alias: {
             components: path.resolve(__dirname, './src/components'),
